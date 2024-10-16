844f2690eb035b83095806b201d959393a9dabc1
Luis Gustavo <luisbarbosa.dev@gmail.com>
2024-10-16 08:52:09 -0300

    fix: lint sintax error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './global.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes'
+
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop' />
+      <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
