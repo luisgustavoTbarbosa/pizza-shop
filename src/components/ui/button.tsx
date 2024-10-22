dc172eae38810ade5a720670e4a09978df0a1074
Luis Gustavo <luisbarbosa.dev@gmail.com>
2024-10-22 07:30:34 -0300

    feat: update profile modal

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        success:
+          'bg-emerald-500 text-white hover:bg-emerald-500/90 dark:bg-emerald-600 dark:hover:bg-emerald-600/80',
       },
       size: {
         default: 'h-9 px-4 py-2',
