73f51f2a19bad97415c332cfe49d6275f92200f5
Luis Gustavo <luisbarbosa.dev@gmail.com>
2024-10-23 20:35:40 -0300

    feat: orders filter

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -2,6 +2,9 @@ import { api } from '@/lib/axios'
 
 export interface GetOrdersQuery {
   pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
 }
 
 interface GetOrdersResponde {
@@ -19,10 +22,18 @@ interface GetOrdersResponde {
   }
 }
 
-export async function getOrders({ pageIndex }: GetOrdersQuery) {
+export async function getOrders({
+  pageIndex,
+  orderId,
+  customerName,
+  status,
+}: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponde>('/orders', {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   })
 
